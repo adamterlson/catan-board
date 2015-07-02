24bd2a4656626f05d1d428dc2cf62f7bb94bb03b
Adam Terlson <adam.terlson@gmail.com>
2015-07-02 13:24:45 -0500

    Border associations

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,13 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 var watch = require('gulp-watch');
+var sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('default', function () {
   return gulp.src('lib/*.js')
     .pipe(watch('lib/*.js'))
+    .pipe(sourcemaps.init({ loadMaps: true }))
     .pipe(babel())
+    .pipe(sourcemaps.write('./'))
     .pipe(gulp.dest('dist'));
 });
