3e50931945225e0055cd4327228aa49b8fdab6b3
Adam Terlson <adam.terlson@gmail.com>
2015-07-04 09:12:17 -0500

    sass

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var babelify = require('babelify');
 var browserify = require('browserify');
 var watchify = require('watchify');
 var source = require('vinyl-source-stream');
+var sass = require('gulp-sass');
 var gulp = require('gulp');
 
 var b = watchify(browserify({
@@ -9,14 +10,27 @@ var b = watchify(browserify({
   debug: true
 }));
 
+
+gulp.task('sass', function () {
+  gulp.src('./css/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./dist/css'));
+});
+ 
+gulp.task('sass:watch', function () {
+  gulp.watch('./css/**/*.scss', ['sass']);
+});
+
+gulp.task('bundle', bundle)
+
 b.on('update', bundle);
 
 function bundle() {
-    return b
+  return b
     .transform(babelify)
     .bundle()
     .pipe(source('main.js'))
     .pipe(gulp.dest('./dist'));
 }
 
-gulp.task('default', bundle);
+gulp.task('default', ['bundle', 'sass:watch']);
